4eced2b03da1faccbaa094367bfa121c694e8505
Simao Dias <simao.daniel.silva.dias@gmail.com>
2023-05-12 23:04:31 +0100

    About e Contacts

--- js/main.js
+++ js/main.js
@@ -185,3 +185,14 @@
 
 })(jQuery);
 
+function initMap() {
+	var location = { lat: 41.59683900 , lng: -8.26850600 };
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: location
+	});
+	var marker = new google.maps.Marker({
+		position: location,
+		map: map
+	});
+}
